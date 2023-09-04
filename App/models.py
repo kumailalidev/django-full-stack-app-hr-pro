e9b6c3b870c4bf8a6cb85d6ee812f56705f97b3e
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-09-04 20:51:40 +0500

    Added age field to Candidate Model also updated form fields inside home.html

--- App/models.py
+++ App/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Candidate(models.Model):
     firstname = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
+    age = models.CharField(max_length=2)
     email = models.EmailField(max_length=50)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
