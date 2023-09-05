e958dc34ffcc79e31fafdb97b7cbea37c91bc392
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-09-05 22:33:59 +0500

    Added support for media files, updated settings.py and urls.py

--- Candidates/urls.py
+++ Candidates/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from App import views
+from Candidates import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name="home"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
