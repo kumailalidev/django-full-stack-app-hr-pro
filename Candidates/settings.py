a523d5c4544a4c030bdb36d18942665f55383a16
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-09-06 01:59:12 +0500

    Added support for django-multiselectfield library

--- Candidates/settings.py
+++ Candidates/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "App",
+    "multiselectfield",
     "django_cleanup",
     "crispy_forms",
     "crispy_bootstrap5",
