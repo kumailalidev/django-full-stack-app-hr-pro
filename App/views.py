f10b9f2778d0bf46541c0f8fa7a19b848e899080
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-09-08 18:34:24 +0500

    Added register, backend and candidate views inside views.py

--- App/views.py
+++ App/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from .forms import CandidateForm
+from .models import Candidate
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 
 
+# FRONTEND
+# Home
 def home(request):
+    return render(request, "home.html")
+
+
+# Candidate registration
+def register(request):
     if request.method == "POST":
         form = CandidateForm(request.POST, request.FILES)
         if form.is_valid():
@@ -12,8 +20,25 @@ def home(request):
             messages.success(request, "Registered Successfully!")
             return HttpResponseRedirect("/")
         else:
-            return render(request, "home.html", {"form": form})
+            return render(request, "register.html", {"form": form})
 
     else:
         form = CandidateForm()
-        return render(request, "home.html", {"form": form})
+        return render(request, "register.html", {"form": form})
+
+
+# BACKEND
+# HR - Home page (backend)
+def backend(request):
+    context = {
+        "data_read": Candidate.objects.all(),
+    }
+    return render(request, "backend.html", context)
+
+
+# Access candidates (individually)
+def candidate(request, id):
+    data = Candidate.objects.get(pk=id)
+    form = CandidateForm(isinstance=data)
+    context = {"form": form}
+    return render(request, "candidate.html", context)
